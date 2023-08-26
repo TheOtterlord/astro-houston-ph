1d51cc9d3f03470e00ffefc08c5b204b87bb60e4
= <otterlord.dev@gmail.com>
2023-08-26 22:42:23 +0100

    Add assets prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   base: '/ph',
+  build: {
+    assetsPrefix: '/ph/_astro'
+  },
   integrations: [tailwind()],
   output: "server",
   adapter: cloudflare()
