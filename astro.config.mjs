99d88fe7740c5607b7f77a01594e05d4092c6d49
= <otterlord.dev@gmail.com>
2023-08-26 22:50:02 +0100

    Change config option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   build: {
-    assetsPrefix: '/ph'
+    assets: 'ph/_astro'
   },
   integrations: [tailwind()],
   output: "server",
