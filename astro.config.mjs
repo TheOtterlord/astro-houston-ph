d2328933333afb68114c9256d6c9acf443e38cad
= <otterlord.dev@gmail.com>
2023-08-26 22:34:13 +0100

    Add base route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/ph',
   integrations: [tailwind()],
   output: "server",
   adapter: cloudflare()
